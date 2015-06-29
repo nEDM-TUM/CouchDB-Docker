252a61aafc288c64230f2af7241025fc6c44845c
Michael Marino <mmarino@gmail.com>
2015-06-29 17:39:26 +0200

    Enable setting the private key with an env variable.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 if [ "$1" = 'couchdb' ]; then
-  export PRIVATE_KEY=`python -c 'import os, binascii, sys; sys.stdout.write(binascii.b2a_hex(os.urandom(30)))'`
+  if [ x"$PRIVATE_KEY" = "x" ]; then
+    export PRIVATE_KEY=`python -c 'import os, binascii, sys; sys.stdout.write(binascii.b2a_hex(os.urandom(30)))'`
+  fi
   sed -e "s/@SECRETREPLACE@/$PRIVATE_KEY/" -e "s/@ADMINHASH@/$ADMINHASH/" /local.ini > /usr/local/etc/couchdb/local.ini
   sed -e "s/@SECRETREPLACE@/$PRIVATE_KEY/" /couchdb_updater.py.in > /couchdb_updater.py
   sed -e "s/@SECRETREPLACE@/$PRIVATE_KEY/" /couchdb_alarm_daemon.py.in > /couchdb_alarm_daemon.py
