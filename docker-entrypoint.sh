8267abc7823da15dcf9da5102b32e77ff48080e2
Michael Marino <mmarino@gmail.com>
2015-07-31 12:34:57 +0200

    Make sure the alarm daemon is executable.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@ if [ "$1" = 'couchdb' ]; then
   sed -e "s/@SECRETREPLACE@/$PRIVATE_KEY/" -e "s/@ADMINHASH@/$ADMINHASH/" /local.ini > /usr/local/etc/couchdb/local.ini
   sed -e "s/@SECRETREPLACE@/$PRIVATE_KEY/" /couchdb_updater.py.in > /couchdb_updater.py
   sed -e "s/@SECRETREPLACE@/$PRIVATE_KEY/" /couchdb_alarm_daemon.py.in > /couchdb_alarm_daemon.py
+  chmod +x /couchdb_alarm_daemon.py
 fi
 
 exec /entrypoint.sh "$@"
